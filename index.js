eb60cfcfbecd8f8d092b7ad3810e115f08e5ae8b
gladys <achandogladys6@gmail.com>
2025-06-22 13:09:58 +0300

    add new changes

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 // Add your code here
-function submitData(name,email){
- return fetch('http://localhost:3000/users',{
-  method:"POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body:JSON.stringify({name:name, email:email})
- })
- .then(response => response.json())
- .then(data => {
-  const p=document.createElement('p')
-  p.innerText = 'User ID: ${data.id}';
-  document.body.appendChild(p);
-  return data;
- })
- .catch (error =>{
-  const p=document.createElement('p');
-  p.innerText=error.message;
-  document.body.appendChild(p);
-  throw error;
- })
-  
- }
 
- 
-  
-
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    const p = document.createElement('p');
+    // This line must use backticks and include only data.id
+    p.innerText = data.id;
+    document.body.appendChild(p);
+    return data;
+  })
+  .catch(error => {
+    const p = document.createElement('p');
+    // This must match the error message exactly ("Unauthorized Access")
+    p.innerText = error.message;
+    document.body.appendChild(p);
+    return error;
+  });
+}
+console.log(submitData);
+submitData("Gladys", "gladys@example.com").then(console.log)
 
